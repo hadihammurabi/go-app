6c7c915652f48bb7f446c52501f6fff3535bd32b
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-09-19 23:15:07 +0700

    add welcome message

--- route.go
+++ route.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"belajar-go-rest-api/model"
-
 	"github.com/gofiber/fiber/v2"
 )
 
 func configureRoute(app *fiber.App) {
 	app.Get("/", func(c *fiber.Ctx) error {
-		users := []model.User{}
-		return c.JSON(users)
+		return c.JSON(fiber.Map{
+			"message": "Selamat datang di Belajar REST API dengan Go",
+		})
 	})
 }
