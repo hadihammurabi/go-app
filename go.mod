8fdd62f769cf78ee4ab7ed33a65efe6a50a91073
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2020-09-19 23:50:28 +0700

    generate uuid and hash password before users creation

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/gofiber/fiber/v2 v2.0.2
 	github.com/joho/godotenv v1.3.0
 	github.com/satori/go.uuid v1.2.0
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
 	gorm.io/driver/postgres v1.0.0
 	gorm.io/gorm v1.20.1
 )
--- model/user.go
+++ model/user.go
@@ -1,8 +1,22 @@
 package model
 
+import (
+	uuid "github.com/satori/go.uuid"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
+
 // User model
 type User struct {
 	Base
 	Email    string `json:"email"`
 	Password string `json:"password"`
+}
+
+// BeforeCreate func
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = uuid.NewV4()
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(u.Password), 10)
+	u.Password = string(hashedPassword)
+	return
 }
