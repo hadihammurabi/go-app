f57c59cf0c9dd3437faeb5652d970545ef6ef3b8
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-07-04 18:50:45 +0700

    feat(util): map to struct

---
+++ util/map.go
@@ -0,0 +1,17 @@
+package util
+
+import "encoding/json"
+
+func MapToStruct(data interface{}, v interface{}) error {
+	jsoned, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(jsoned, v)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
