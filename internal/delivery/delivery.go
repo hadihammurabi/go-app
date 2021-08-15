9d37ad6d5f5b4347d2fa1eb4dca2b231ea8f39d8
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-08-15 11:50:41 +0700

    refactor: remove delivery mq

--- internal/delivery/delivery.go
+++ internal/delivery/delivery.go
@@ -1,29 +1,22 @@
 package delivery
 
 import (
-	"github.com/hadihammurabi/belajar-go-rest-api/internal/delivery/mq"
 	"github.com/hadihammurabi/belajar-go-rest-api/internal/delivery/rest"
 	"github.com/hadihammurabi/belajar-go-rest-api/pkg/util/di"
 )
 
 type Delivery struct {
 	Rest *rest.Delivery
-	MQ   *mq.Delivery
 }
 
 func NewDelivery(ioc di.IOC) *Delivery {
 	deliveryRest := rest.Init(ioc)
-	deliveryMQ := mq.Init(ioc)
 
 	return &Delivery{
 		Rest: deliveryRest,
-		MQ:   deliveryMQ,
 	}
 }
 
 func (d Delivery) Run() {
 	go d.Rest.Run()
-	if d.MQ != nil {
-		go d.MQ.Run()
-	}
 }
