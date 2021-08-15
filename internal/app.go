cdd69a4a405e01a6438e5e89a3d4d03935c29253
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-08-15 11:33:05 +0700

    refactor: remove repo and service from ioc to app

--- internal/app.go
+++ internal/app.go
@@ -2,14 +2,21 @@ package internal
 
 import (
 	"github.com/hadihammurabi/belajar-go-rest-api/internal/delivery"
+	"github.com/hadihammurabi/belajar-go-rest-api/internal/repository"
+	"github.com/hadihammurabi/belajar-go-rest-api/internal/service"
 	"github.com/hadihammurabi/belajar-go-rest-api/pkg/util/di"
 )
 
 type App struct {
-	Delivery *delivery.Delivery
+	Delivery   *delivery.Delivery
+	Service    *service.Service
+	Repository *repository.Repository
 }
 
 func NewApp(ioc di.IOC) *App {
+	ioc[di.DI_REPOSITORY] = repository.NewRepository(ioc)
+	ioc[di.DI_SERVICE] = service.NewService(ioc)
+
 	return &App{
 		Delivery: delivery.NewDelivery(ioc),
 	}
--- ioc.go
+++ ioc.go
@@ -3,8 +3,6 @@ package main
 import (
 	"github.com/hadihammurabi/belajar-go-rest-api/config"
 	"github.com/hadihammurabi/belajar-go-rest-api/internal"
-	"github.com/hadihammurabi/belajar-go-rest-api/internal/repository"
-	"github.com/hadihammurabi/belajar-go-rest-api/internal/service"
 	"github.com/hadihammurabi/belajar-go-rest-api/pkg/util/di"
 )
 
@@ -12,8 +10,6 @@ import (
 func NewIOC(conf *config.Config) di.IOC {
 	ioc := di.IOC{}
 	ioc[di.DI_CONFIG] = conf
-	ioc[di.DI_REPOSITORY] = repository.NewRepository(ioc)
-	ioc[di.DI_SERVICE] = service.NewService(ioc)
 	ioc[di.DI_APP] = internal.NewApp(ioc)
 	return ioc
 }
