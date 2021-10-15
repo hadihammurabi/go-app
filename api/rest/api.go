c7f2a93b197d7ec3ad0f1ca251bb47368a5a7089
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2021-10-15 22:56:20 +0700

    refactor: move route to api declaration

--- api/rest/api.go
+++ api/rest/api.go
@@ -59,3 +59,15 @@ func (d *APIRest) Stop() {
 	d.HTTP.Shutdown()
 	log.Println("Server was stopped")
 }
+
+// ConfigureRoute func
+func (api *APIRest) ConfigureRoute() {
+	api.HTTP.Get("/", func(c *fiber.Ctx) error {
+		return c.JSON(fiber.Map{
+			"message": "Selamat datang di Belajar REST API dengan Go",
+		})
+	})
+
+	NewAuthHandler(api)
+	NewUserHandler(api)
+}
