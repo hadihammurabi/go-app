4c8be9bf5a3cbd06d1a1ae0a425691d09c34b843
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2022-11-13 19:35:14 +0700

    refactor: preparation in runner

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"os"
-	"runtime"
-	"syscall"
 
 	"github.com/hadihammurabi/belajar-go-rest-api/api/rest"
 	"github.com/hadihammurabi/belajar-go-rest-api/config"
@@ -11,32 +9,17 @@ import (
 	"github.com/hadihammurabi/belajar-go-rest-api/internal"
 	"github.com/hadihammurabi/belajar-go-rest-api/util/runner"
 	"github.com/hadihammurabi/go-ioc/ioc"
-	"github.com/spf13/viper"
 
 	"net/http"
 	_ "net/http/pprof"
 )
 
 func init() {
-	configLocation := os.Getenv("APP_CONFIG")
-	viper.SetConfigType("yaml")
-	viper.SetConfigFile(configLocation)
-	if err := viper.ReadInConfig(); err != nil {
-		panic(err)
-	}
+	runner.PrepareRuntime()
+	runner.PrepareConfig()
 }
 
 func main() {
-	runtime.GOMAXPROCS(runtime.NumCPU())
-	var rLimit syscall.Rlimit
-	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
-		panic(err)
-	}
-	rLimit.Cur = rLimit.Max
-	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
-		panic(err)
-	}
-
 	conf, err := config.New()
 	if err != nil {
 		panic(err)
--- util/runner/runner.go
+++ util/runner/runner.go
@@ -3,8 +3,37 @@ package runner
 import (
 	"os"
 	"os/signal"
+	"runtime"
 	"syscall"
+
+	"github.com/spf13/viper"
 )
+
+func PrepareRuntime() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	var rLimit syscall.Rlimit
+	if err := syscall.Getrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+	rLimit.Cur = rLimit.Max
+	if err := syscall.Setrlimit(syscall.RLIMIT_NOFILE, &rLimit); err != nil {
+		panic(err)
+	}
+}
+
+func PrepareConfig() {
+	configLocation := os.Getenv("APP_CONFIG")
+
+	if configLocation == "" {
+		configLocation = "config.yaml"
+	}
+
+	viper.SetConfigType("yaml")
+	viper.SetConfigFile(configLocation)
+	if err := viper.ReadInConfig(); err != nil {
+		panic(err)
+	}
+}
 
 func GracefulStop(gracefulStop chan os.Signal, callback func()) {
 	signal.Notify(gracefulStop, syscall.SIGTERM)
