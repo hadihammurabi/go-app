9b6ab94644a0aa8a6b59f8914c03e4b58ed45251
Hadi Hidayat Hammurabi <hadihammurabi@gmail.com>
2023-01-24 23:11:00 +0700

    fix(cache): not have one choosen driver

--- pkg/cache/cache.go
+++ pkg/cache/cache.go
@@ -1,24 +1,10 @@
 package cache
 
 import (
+	"github.com/gowok/gowok/cache"
 	"github.com/gowok/gowok/driver"
 )
 
-type Driver string
-
-const (
-	DriverRedis Driver = "redis"
-)
-
-type Config struct {
-	Driver Driver
-	URL    string
-}
-
-func New(config Config) (driver.Cache, error) {
-	if config.Driver == DriverRedis {
-		return ConfigureRedis(config)
-	}
-
-	return ConfigureDefault(config)
+func New() (driver.Cache, error) {
+	return cache.NewMap()
 }
