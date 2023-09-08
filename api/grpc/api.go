ab1309126d2ba1a7d1b4976f85c2107741894606
hadihammurabi <hadihammurabi@gmail.com>
2023-09-09 01:13:42 +0700

    refactor(api): remove config and service from grpc

--- api/grpc/api.go
+++ api/grpc/api.go
@@ -4,32 +4,25 @@ import (
 	"log"
 	"net"
 
-	"github.com/gowok/gowok"
 	"github.com/hadihammurabi/belajar-go-rest-api/api/grpc/index"
 	"github.com/hadihammurabi/belajar-go-rest-api/driver"
-	"github.com/hadihammurabi/belajar-go-rest-api/service"
 	"google.golang.org/grpc"
 )
 
 // APIGrpc struct
 type APIGrpc struct {
-	Config  *gowok.Config
-	Service *service.Service
-
 	Grpc *grpc.Server
 }
 
 var a *APIGrpc
 
 func NewAPIGrpc() *APIGrpc {
-	conf := driver.Get().Config
-	sv := service.Get()
-
 	api := &APIGrpc{
-		Config:  conf,
-		Service: sv,
-		Grpc:    grpc.NewServer(),
+		Grpc: grpc.NewServer(),
 	}
+
+	index.RegisterIndexServer(api.Grpc, index.New())
+
 	return api
 }
 
@@ -43,18 +36,17 @@ func Get() *APIGrpc {
 }
 
 func (d *APIGrpc) Run() {
-	if !d.Config.App.Grpc.Enabled {
+	grpcConf := driver.Get().Config.App.Grpc
+	if !grpcConf.Enabled {
 		return
 	}
 
-	index.RegisterIndexServer(d.Grpc, index.New())
-
-	listen, err := net.Listen("tcp", d.Config.App.Grpc.Host)
+	listen, err := net.Listen("tcp", grpcConf.Host)
 	if err != nil {
 		panic(err)
 	}
 
-	log.Println("API GRPC started at", d.Config.App.Grpc.Host)
+	log.Println("API GRPC started at", grpcConf.Host)
 	err = d.Grpc.Serve(listen)
 	if err != nil {
 		panic(err)
