9bef82d245f5b4960af5fb9c763c85ec3bd6dfa3
hadihammurabi <hadihammurabi@gmail.com>
2023-09-09 01:17:54 +0700

    refactor(api): move grpc initializer into driver

--- api/grpc/api.go
+++ api/grpc/api.go
@@ -1,59 +1,14 @@
 package grpc
 
-import (
-	"log"
-	"net"
+import "github.com/hadihammurabi/belajar-go-rest-api/driver/api"
 
-	"github.com/hadihammurabi/belajar-go-rest-api/api/grpc/index"
-	"github.com/hadihammurabi/belajar-go-rest-api/driver"
-	"google.golang.org/grpc"
-)
+var a *api.APIGrpc
 
-// APIGrpc struct
-type APIGrpc struct {
-	Grpc *grpc.Server
-}
-
-var a *APIGrpc
-
-func NewAPIGrpc() *APIGrpc {
-	api := &APIGrpc{
-		Grpc: grpc.NewServer(),
-	}
-
-	index.RegisterIndexServer(api.Grpc, index.New())
-
-	return api
-}
-
-func Get() *APIGrpc {
+func Get() *api.APIGrpc {
 	if a != nil {
 		return a
 	}
 
-	a = NewAPIGrpc()
+	a = api.NewAPIGrpc()
 	return a
-}
-
-func (d *APIGrpc) Run() {
-	grpcConf := driver.Get().Config.App.Grpc
-	if !grpcConf.Enabled {
-		return
-	}
-
-	listen, err := net.Listen("tcp", grpcConf.Host)
-	if err != nil {
-		panic(err)
-	}
-
-	log.Println("API GRPC started at", grpcConf.Host)
-	err = d.Grpc.Serve(listen)
-	if err != nil {
-		panic(err)
-	}
-}
-
-func (d *APIGrpc) Stop() {
-	d.Grpc.GracefulStop()
-	log.Println("GRPC was stopped")
 }
---
+++ driver/api/grpc.go
@@ -0,0 +1,48 @@
+package api
+
+import (
+	"log"
+	"net"
+
+	"github.com/hadihammurabi/belajar-go-rest-api/api/grpc/index"
+	"github.com/hadihammurabi/belajar-go-rest-api/driver"
+	"google.golang.org/grpc"
+)
+
+// APIGrpc struct
+type APIGrpc struct {
+	Grpc *grpc.Server
+}
+
+func NewAPIGrpc() *APIGrpc {
+	api := &APIGrpc{
+		Grpc: grpc.NewServer(),
+	}
+
+	index.RegisterIndexServer(api.Grpc, index.New())
+
+	return api
+}
+
+func (d *APIGrpc) Run() {
+	grpcConf := driver.Get().Config.App.Grpc
+	if !grpcConf.Enabled {
+		return
+	}
+
+	listen, err := net.Listen("tcp", grpcConf.Host)
+	if err != nil {
+		panic(err)
+	}
+
+	log.Println("API GRPC started at", grpcConf.Host)
+	err = d.Grpc.Serve(listen)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func (d *APIGrpc) Stop() {
+	d.Grpc.GracefulStop()
+	log.Println("GRPC was stopped")
+}
