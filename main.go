2fc0cea1955dcd249c409cdb276b6b551b756884
hadihammurabi <hadihammurabi@gmail.com>
2023-11-29 15:32:41 +0700

    fix(main): ignite grpc

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/gowok/gowok"
+	"github.com/hadihammurabi/belajar-go-rest-api/api/grpc"
 	"github.com/hadihammurabi/belajar-go-rest-api/api/messaging"
 	"github.com/hadihammurabi/belajar-go-rest-api/api/web"
 )
@@ -10,6 +11,7 @@ func main() {
 	project := gowok.Get()
 	project.Runner.AddRunFunc(func() {
 		web.ConfigureRoute()
+		grpc.ConfigureServices()
 		messaging.ConfigureMessage()
 	})
 	project.Runner.Run()
