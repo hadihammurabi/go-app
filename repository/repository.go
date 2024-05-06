aa78cf94d36b6ac0cac7ad43cefa3f94a79aadca
hadihammurabi <hadihammurabi@gmail.com>
2024-05-06 08:27:24 +0700

    fix(repository): use gowok singleton

--- repository/repository.go
+++ repository/repository.go
@@ -1,21 +1,14 @@
 package repository
 
+import "github.com/gowok/gowok"
+
 // Repository struct
 type Repository struct {
 }
 
-var r *Repository
-
 // NewRepository func
-func NewRepository() *Repository {
-	return &Repository{}
+func NewRepository() Repository {
+	return Repository{}
 }
 
-func Get() *Repository {
-	if r != nil {
-		return r
-	}
-
-	r = NewRepository()
-	return r
-}
+var Get = gowok.Singleton(NewRepository)
